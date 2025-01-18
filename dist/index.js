e56c2926c1ace7ab29afaa0dfbf73b0df01d2e22
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-01-18 23:48:55 +0530

    continuos message in 2 second interval

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws) {
+    console.log("new connection");
     ws.on('error', console.error);
     ws.on('message', function message(data) {
         console.log('received: %s', data);
     });
+    setInterval(() => {
+        ws.send("what is the price of solana" + Date.now());
+    }, 2000);
     ws.send('something');
 });
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+    console.log("new connection")
 
-  ws.on('message', function message(data) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
+
+  setInterval(()=>{
+    ws.send("what is the price of solana"+Date.now())
+
+  },2000)
 
   ws.send('something');
 });
